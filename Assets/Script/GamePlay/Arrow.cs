0479fd3d644c615b6cd879c28ca7fb98b0d97895
Phạm Mạnh Tuấn <manhtuan6476@gmail.com>
2021-09-16 14:44:54 +0700

    change gameplay v3

--- Assets/Script/GamePlay/Arrow.cs
+++ Assets/Script/GamePlay/Arrow.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Arrow : MonoBehaviour
 {
@@ -13,7 +14,8 @@ public class Arrow : MonoBehaviour
        
     private float currentSpeed;
     private bool active;
-    private bool frist;
+    private bool first;
+    private bool first1;
 
     private void OnEnable()
     {
@@ -24,9 +26,11 @@ public class Arrow : MonoBehaviour
     {
         this.type = type;
         img.sprite = spr[type];
+        img.color = new Color(1, 1, 1, 1);
         currentSpeed = speedMoveDefault * speed;
         active = true;
-        frist = true;
+        first = true;
+        first1 = true;
     }
     // Update is called once per frame
     void Update()
@@ -35,19 +39,26 @@ public class Arrow : MonoBehaviour
             trans.Translate(0, currentSpeed * Time.deltaTime, 0);
         if (trans.position.y > 0 && active)
         {
-            if (frist)
+            if (first)
             {
-                frist = false;
+                first = false;
                 PlayScreen.Instance.OnEnemyHit(type);
             } else
             {
-                if (trans.position.y > 2f)
+                if (trans.position.y > 2f && first1)
                 {
-                    active = false;
+                    //  active = false;
+                    first1 = false;
+                    img.DOPause();
+                    img.DOColor(new Color(1, 1, 1, 0), 0.2f);
                     EvenGlobalManager.Instance.OnArrowDisappear.Dispatch(this);
-                    SimplePool.Despawn(gameObject);
+                  //  SimplePool.Despawn(gameObject);
                 }
             }          
         }
+    }
+    public void SetActive(bool active)
+    {
+        this.active = active;
     }
 }
